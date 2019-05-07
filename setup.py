3ac3bc48748d23cbc4080b12483ba66a7a561135
Guy Korland <gkorland@gmail.com>
2019-05-07 10:25:51 +0300

    bump version to 0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='redistimeseries',
-    version='0.1',
+    version='0.2',
 
     description='RedisTimeSeries Python Client',
     url='https://github.com/RedisTimeSeries/redistimeseries-py',
