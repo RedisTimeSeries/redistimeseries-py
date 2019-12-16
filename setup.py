4ff9c8bf1690efe9d030267e7f0d30b9b7ce08e0
Guy Korland <gkorland@gmail.com>
2019-12-16 15:14:06 +0200

    add long_description

--- setup.py
+++ setup.py
@@ -1,19 +1,30 @@
 from setuptools import setup, find_packages
+import io
+
+def read_all(f):
+    with io.open(f, encoding="utf-8") as I:
+        return I.read()
+
+requirements = map(str.strip, open("requirements.txt").readlines())
+
 setup(
     name='redistimeseries',
     version='0.5',
-
     description='RedisTimeSeries Python Client',
+    long_description=read_all("README.md"),
+    long_description_content_type='text/markdown',
     url='https://github.com/RedisTimeSeries/redistimeseries-py',
     packages=find_packages(),
-    install_requires=['redis'],
-
+    install_requires=requirements,
     classifiers=[
-        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 2.7',
         'Topic :: Database',
         'Topic :: Software Development :: Testing'
-    ]
+    ],
+    keywords='Redis TimeSeries Extension',
+    author='RedisLabs',
+    author_email='oss@redislabs.com',
+    test_suite="tests"
 )
