9a1220923b0536596408ff4e2529acf784c5da88
Guy Korland <gkorland@gmail.com>
2019-12-20 20:21:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ requirements = map(str.strip, open("requirements.txt").readlines())
 
 setup(
     name='redistimeseries',
-    version='0.5',
+    version='0.6.0',
     description='RedisTimeSeries Python Client',
     long_description=read_all("README.md"),
     long_description_content_type='text/markdown',
