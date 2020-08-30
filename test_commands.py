190178972f1bbc04e49de4810145d3ede469ebd3
filipecosta90 <filipecosta.90@gmail.com>
2020-08-30 22:24:37 +0100

    [add] Added support to specify the chunk size in Bytes of a time-series on create, add, incrby, and decrby methods

--- test_commands.py
+++ test_commands.py
@@ -24,6 +24,11 @@ class RedisTimeSeriesTest(TestCase):
         info = rts.info(4)
         self.assertEqual(20, info.retention_msecs)
         self.assertEqual('Series', info.labels['Time'])
+        # Test for a chunk size of 128 Bytes
+        self.assertTrue(rts.create("time-serie-1",chunk_size=128))
+        info = rts.info("time-serie-1")
+        self.assertEqual(128, info.chunk_size)
+
 
     def testAlter(self):
         '''Test TS.ALTER calls'''
@@ -52,6 +57,11 @@ class RedisTimeSeriesTest(TestCase):
         self.assertEqual(10, info.retention_msecs)
         self.assertEqual('Labs', info.labels['Redis'])
 
+        # Test for a chunk size of 128 Bytes on TS.ADD
+        self.assertTrue(rts.add("time-serie-1", 1, 10.0, chunk_size=128))
+        info = rts.info("time-serie-1")
+        self.assertEqual(128, info.chunk_size)
+
     def testMAdd(self):
         '''Test TS.MADD calls'''
 
@@ -76,6 +86,16 @@ class RedisTimeSeriesTest(TestCase):
         self.assertEqual((7, 3.75), rts.get(2))
         self.assertTrue(rts.decrby(2, 1.5, timestamp=15))
         self.assertEqual((15, 2.25), rts.get(2))
+
+        # Test for a chunk size of 128 Bytes on TS.INCRBY
+        self.assertTrue(rts.incrby("time-serie-1", 10, chunk_size=128))
+        info = rts.info("time-serie-1")
+        self.assertEqual(128, info.chunk_size)
+
+        # Test for a chunk size of 128 Bytes on TS.DECRBY
+        self.assertTrue(rts.decrby("time-serie-2", 10, chunk_size=128))
+        info = rts.info("time-serie-2")
+        self.assertEqual(128, info.chunk_size)
 
     def testCreateRule(self):
         '''Test TS.CREATERULE and TS.DELETERULE calls'''
