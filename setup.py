396812ed78fb47c52c5a06b31a3d132571e88abb
filipecosta90 <filipecosta.90@gmail.com>
2020-10-01 22:21:59 +0100

    Version bump to 1.4.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ requirements = list(map(str.strip, open("requirements.txt").readlines()))
 
 setup(
     name='redistimeseries',
-    version="1.4.0",
+    version="1.4.1",
     description='RedisTimeSeries Python Client',
     long_description=read_all("README.md"),
     long_description_content_type='text/markdown',
