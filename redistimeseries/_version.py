97bb7bf3d35b565895a397d62153bf0780cab7ba
Chayim I. Kirshen <c@kirshen.com>
2021-05-23 10:44:23 +0300

    modern build - first

--- redistimeseries/_version.py
+++ redistimeseries/_version.py
@@ -1,4 +1,12 @@
-
 # This attribute is the only one place that the version number is written down,
 # so there is only one place to change it when the version number changes.
-__version__ = "1.4.3"
+# Edit the pyproject.toml to modify versions
+def get_version():
+    import importlib.metadata
+    try:
+        md = importlib.metadata.metadata('redistimeseries')
+        return md['Version']
+    except (IndexError, KeyError, importlib.metadata.PackageNotFoundError):
+        return 'dev'
+
+__version__ = get_version()
