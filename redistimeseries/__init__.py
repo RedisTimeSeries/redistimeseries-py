6e657bcafd371536e9ee9c399e199a9e95559f0b
dvora-h <67596500+dvora-h@users.noreply.github.com>
2022-04-11 14:47:32 +0300

    add deprecation warning

--- redistimeseries/__init__.py
+++ redistimeseries/__init__.py
@@ -1,1 +1,5 @@
 from ._version import __version__
+from warnings import warn
+warn("Please upgrade to redis-py (https://pypi.org/project/redis/) "
+"This library is deprecated, and all features have been merged into redis-py.",
+DeprecationWarning, stacklevel=2)
